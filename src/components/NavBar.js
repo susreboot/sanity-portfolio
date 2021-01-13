dff4e18b03cfe57395ed3b1184e24cc632582502
susreboot <joshbrade@gmail.com>
2021-01-12 21:40:47 -0500

    added NavBar component (with Links)  & SocialIcons -- changed react version to ^16.0 to deal with react-social-icons package error

---
+++ src/components/NavBar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
+
+export default function NavBar() {
+    return (
+        <header className="bg-red-600">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink 
+                    to="/" 
+                    exact 
+                    className="inline-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+                    activeClassName="text-white"
+                    >
+                        Home
+                    </NavLink>
+                    <NavLink 
+                    to="/post" 
+                    className="inline-flex items-center py-3 px-3 my-6 rounder text-red-200 hover:text-green-800" 
+                    activeClassName="text-red-100 bg-red-700 rounded-2xl"
+                    >
+                        Blog
+                    </NavLink>
+                    <NavLink 
+                    to="/project"
+                    className="inline-flex items-center py-3 px-3 my-6 rounder text-red-200 hover:text-green-800"
+                    activeClassName="text-red-100 bg-red-700 rounded-2xl"
+                    >
+                        Projects
+                    </NavLink>
+                    <NavLink 
+                    to="/about"
+                    className="inline-flex items-center py-3 px-3 my-6 rounder text-red-200 hover:text-green-800"
+                    activeClassName="text-red-100 bg-red-700 rounded-2xl"
+                    >
+                        About
+                    </NavLink>
+                </nav>
+                <div>
+                    <SocialIcon url="" className="mr-4" target="_blank" fgColor="#fff" style={{height:35, width: 35}}/>
+                    <SocialIcon url="" className="mr-4" target="_blank" fgColor="#fff" style={{height:35, width: 35}}/>
+                </div>
+            </div>
+        </header>
+    )
+}
